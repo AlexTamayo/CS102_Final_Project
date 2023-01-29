4f7a803215dd8948a270e97b27179ebc6ff81394
AlexTamayo <catd00@gmail.com>
2023-01-28 21:46:54 -0800

    Create nintendo_switch_games_list.py
    First commit of Nintendo Switch games list

---
+++ nintendo_switch_games_list.py
@@ -0,0 +1,6 @@
+'''
+This is a list of all of my Nintendo Switch games with their respective name, release date (US), Publisher, Developer, Age rating and Categories.
+
+Last updated:
+2023 01 28
+'''
