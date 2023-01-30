fc4f26cd1da90e1f15725cc288558183857031f7
AlexTamayo <catd00@gmail.com>
2023-01-29 17:28:17 -0800

    20230129 1727 Update
    Created a class for the game information and added all the games I wanted to add to the list. I may add others later.

---
+++ NintendoSwithGame.py
@@ -0,0 +1,65 @@
+from datetime import date, datetime
+
+class NintendoSwitchGame:
+    def __init__(self, name, release_date, publisher, developer, single_player_and_cat = None, multiplayer_type = None, age_rating = None):
+        self.name = name #String
+        #This is a list. The first element in the list is the North American release date. Second element should be Japan
+        self.release_date = release_date
+        self.publisher = publisher #String
+        self.developer = developer #This should be a list as there are multiple developers some times.
+        self.single_player_and_cat = single_player_and_cat #This is a list, will return True if there's anything in it.
+        self.multiplayer_type = multiplayer_type #This is a list, will return True if there's anything in it.
+        self.age_rating = age_rating
+    
+    def __repr__(self):
+        return self.name
+
+    def NA_release_date(self):
+        dt = datetime(*self.release_date[0])
+        return dt.strftime("%d %B %Y")
+        # return date(*self.release_date[0])
+
+    def JP_release_date(self):
+        dt = datetime(*self.release_date[1])
+        return dt.strftime("%d %B %Y")
+
+    def is_single_player(self):
+        return bool(self.single_player_and_cat)
+
+    def single_p_print(self):
+        if self.is_single_player():
+            return "Single Player"
+
+    def is_multi_player(self):
+        return bool(self.multiplayer_type)
+
+    def multi_p_print(self):
+        if self.is_multi_player():
+            return "Multiplayer"
+
+    def type_of_game(self):
+        categories_list = []
+        if self.is_single_player():
+            categories_list.append(self.single_p_print())
+            for each in self.single_player_and_cat:
+                categories_list.append(each)
+        if self.is_multi_player():
+            categories_list.append(self.multi_p_print())
+            for each in self.multiplayer_type:
+                categories_list.append(each)
+        return categories_list
+
+
+
+
+if __name__ == "__main__":
+    game_00001 = NintendoSwitchGame("The Legend of Zelda: Breath of the Wild", [[2017, 3, 3]],"Nintendo" ,"Nintendo EPD", ["Action-Adventure"])
+    game_00002 = NintendoSwitchGame("Mario Kart 8 Deluxe", [[2017, 4, 28]],"Nintendo" ,"Nintendo EPD", ["Kart racing"], ["Split Screen", "Online"])
+    game_00003 = NintendoSwitchGame("ARMS", [[2017, 6, 16]],"Nintendo" ,"Nintendo EPD", ["Fighting", "Sports"], ["Split Screen"])
+    game_00004 = NintendoSwitchGame("Astral Chain", [[2019, 8, 30]],"Nintendo" ,"PlatinumGames", ["Action-adventure", "hack and slash"], ["Co-Op"])
+    game_00005 = NintendoSwitchGame("Daemon X Machina", [[2019, 9, 13]],"Nintendo" ,"Marvelous First Studio", ["Action", "Third-person shooter"], ["PvP", "Online"])
+    game_00006 = NintendoSwitchGame("Fire Emblem: Three Houses", [[2019, 7, 29]],"Nintendo" ,"Intelligent Systems", ["Tactical role-playing"])
+    game_00007 = NintendoSwitchGame("Fire Emblem Warriors: Three Hopes", [[2022, 6, 24]],"Nintendo" ,"Omega Force", ["Hack and slash", "Action role-playing", "Real-time tactics"], ["Split Screen", "Co-Op"])
+    game_00008 = NintendoSwitchGame("Hyrule Warriors: Age of Calamity", [[2020, 11, 20]],"Nintendo" ,"Omega Force", ["Hack and slash", "Action role-playing"], ["Split Screen", "Co-Op"])
+    game_00009 = NintendoSwitchGame("Kirby and the Forgotten Land", [[2022, 3, 25]],"Nintendo" ,"HAL Laboratory", ["Platform"], ["Co-Op"])
+    print(game_00007.type_of_game())
--- nintendo_switch_games_list.py
+++ nintendo_switch_games_list.py
@@ -3,6 +3,59 @@ This is a list of all of my Nintendo Switch games with their respective name, re
 
 Last updated:
 2023 01 28
-'''
 
-my_games_list = [["The Legend of Zelda: Breath of the Wild", [2017, 3, 3],"Nintendo" ,"Nintendo EPD", ["Single player" ,"Action-Adventure"]]]
+Here's a list of all Nintendo Swith games
+https://en.wikipedia.org/wiki/List_of_Nintendo_Switch_games_(0%E2%80%939_and_A)#0%E2%80%939
+'''
+from NintendoSwithGame import NintendoSwitchGame
+
+my_games_list = []
+
+my_games_list.append(NintendoSwitchGame("ARMS", [[2017, 6, 16]], ["Nintendo"], ["Nintendo EPD"], ["Fighting", "Sports"], ["Split Screen", "Online"]))
+my_games_list.append(NintendoSwitchGame("Astral Chain", [[2019, 8, 30]], ["Nintendo"], ["PlatinumGames"], ["Action-adventure", "Hack and Slash"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Daemon X Machina", [[2019, 9, 13]], ["Nintendo"], ["Marvelous First Studio"], ["Action", "Third-person shooter"], ["PvP", "Online"]))
+my_games_list.append(NintendoSwitchGame("Fire Emblem: Three Houses", [[2019, 7, 29]],"Nintendo", ["Intelligent Systems"], ["Tactical role-playing"]))
+my_games_list.append(NintendoSwitchGame("Fire Emblem Warriors: Three Hopes", [[2022, 6, 24]], ["Nintendo"], ["Omega Force"], ["Hack and Slash", "Action role-playing", "Real-time tactics"], ["Split Screen", "Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Hyrule Warriors: Age of Calamity", [[2020, 11, 20]], ["Nintendo"], ["Omega Force"], ["Hack and Slash", "Action role-playing"], ["Split Screen", "Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Kirby and the Forgotten Land", [[2022, 3, 25]], ["Nintendo"], ["HAL Laboratory"], ["Platform"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("The Legend of Zelda: Breath of the Wild", [[2017, 3, 3]], ["Nintendo"], ["Nintendo EPD"], ["Action-Adventure"]))
+my_games_list.append(NintendoSwitchGame("The Legend of Zelda: Link's Awakening", [[2019, 9, 20]], ["Nintendo"], ["Grezzo"], ["Action-adventure"]))
+my_games_list.append(NintendoSwitchGame("The Legend of Zelda: Skyward Sword HD", [[2019, 7, 16]], ["Nintendo"], ["Tantalus Media"], ["Action-adventure"]))
+my_games_list.append(NintendoSwitchGame("Luigi's Mansion 3", [[2019, 10, 31]], ["Nintendo"], ["Next Level Games"], ["Action-adventure"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Mario Golf: Super Rush", [[2021, 6, 25]], ["Nintendo"], ["Camelot Software Planning"], ["Sports"], ["Split Screen"]))
+my_games_list.append(NintendoSwitchGame("Mario Kart 8 Deluxe", [[2017, 4, 28]], ["Nintendo"], ["Nintendo EPD"], ["Kart racing"], ["Split Screen", "Online"]))
+my_games_list.append(NintendoSwitchGame("Mario Strikers: Battle League", [[2022, 6, 10]], ["Nintendo"], ["Next Level Games"], ["Sports"], ["Split Screen", "Online"]))
+my_games_list.append(NintendoSwitchGame("Metroid Dread", [[2021, 10, 8]], ["Nintendo"], ["MercurySteam", "Nintendo EPD"], ["Action-adventure", "Metroidvania"]))
+my_games_list.append(NintendoSwitchGame("Nintendo Labo - Toy-Con 04: VR Kit", [[2019, 4, 12]], ["Nintendo"], ["Nintendo EPD"], ["Construction"]))
+my_games_list.append(NintendoSwitchGame("Nintendo Switch Sports", [[2022, 4, 29]], ["Nintendo"], ["Sports"], ["Split Screen", "Online"]))
+my_games_list.append(NintendoSwitchGame("Ori Collector's Edition: Ori the Collection", [[2021, 10, 21]], ["Xbox Game Studios"], ["Moon Studios"], ["Platform-adventure", "Metroidvania"]))
+my_games_list.append(NintendoSwitchGame("Pikmin 3", [[2020, 10, 30]], ["Nintendo"], ["Nintendo EAD", "Eighting"], ["Real-time strategy", "puzzle"], ["Split Screen", "Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Pokémon: Let's Go, Pikachu!", [[2018, 11, 16]], ["Nintendo", "The Pokémon Company"], ["Game Freak"], ["Role-playing"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Pokémon Sword", [[2019, 11, 15]], ["Nintendo", "The Pokémon Company"], ["Game Freak"], ["Role-playing"], ["Online"]))
+my_games_list.append(NintendoSwitchGame("Pokémon Legends: Arceus", [[2022, 1, 28]], ["Nintendo", "The Pokémon Company"], ["Game Freak"], ["Action role-playing"]))
+my_games_list.append(NintendoSwitchGame("Pokkén Tournament", [[2017, 9, 22]], ["Nintendo", "The Pokémon Company"], ["Bandai Namco Studios"], ["Fighting"], ["Split Screen", "Online"]))
+my_games_list.append(NintendoSwitchGame("Splatoon 3", [[2022, 9, 9]], ["Nintendo"], ["Nintendo EPD"], ["Third-person shooter"], ["Online"]))
+my_games_list.append(NintendoSwitchGame("Super Mario 3D All-Stars", [[2020, 9, 18]], ["Nintendo"], ["Nintendo EPD", "1-Up Studio", "Nintendo European Research & Development"], ["Platform", "Action-Adventure"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Super Mario 3D World + Bowser's Fury", [[2021, 2, 12]], ["Nintendo"], ["Nintendo EAD Tokyo["], ["Platform", "Action-adventure"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Super Mario Odyssey", [[2017, 10, 27]], ["Nintendo"], ["Nintendo EPD"], ["Platform", "Action-adventure"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Super Smash Bros. Ultimate", [[2018, 12, 7]], ["	Nintendo"], ["Bandai Namco Studios", "Sora Ltd."], ["Fighting"], ["Split Screen"]))
+my_games_list.append(NintendoSwitchGame("The Wonderful 101", [[2020, 5, 19]], ["PlatinumGames"], ["PlatinumGames"], ["Action-adventure", "Hack and Slash", "Beat 'em up"], ["Co-Op"]))
+my_games_list.append(NintendoSwitchGame("Xenoblade Chronicles: Definitive Edition", [[2020, 5, 29]], ["Nintendo"], ["Monolith Soft"], ["Action role-playing"]))
+my_games_list.append(NintendoSwitchGame("Xenoblade Chronicles 2", [[2017, 12, 1]], ["Nintendo"], ["Monolith Soft"], ["Action role-playing"]))
+my_games_list.append(NintendoSwitchGame("Xenoblade Chronicles 2: Torna – The Golden Country", [[2018, 9, 14]], ["Nintendo"], ["Monolith Soft"], ["Action role-playing"]))
+my_games_list.append(NintendoSwitchGame("Xenoblade Chronicles 3", [[2022, 7, 29]], ["Nintendo"], ["Monolith Soft"], ["Action role-playing"]))
+# my_games_list.append(NintendoSwitchGame("", [[, , ]], [""], [""], [""]))
+# my_games_list.append(NintendoSwitchGame("", [[, , ]], [""], [""], [""]))
+# my_games_list.append(NintendoSwitchGame("", [[, , ]], [""], [""], [""]))
+# my_games_list.append(NintendoSwitchGame("", [[, , ]], [""], [""], [""]))
+# my_games_list.append(NintendoSwitchGame("", [[, , ]], [""], [""], [""]))
+# my_games_list.append(NintendoSwitchGame("", [[, , ]], [""], [""], [""]))
+
+
+
+if __name__ == "__main__":
+    counter = 0
+    for i in my_games_list:
+        print(i.type_of_game())
+        counter += 1
+    # print(counter)
