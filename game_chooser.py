3062b1fad1ba0347c5742a26bf096dd9c33b9219
AlexTamayo <catd00@gmail.com>
2023-01-29 22:57:01 -0800

    20230129 2256 Update
    Made some small modifications to one of the helper functions and outlined a To Do list in the main script.

--- game_chooser.py
+++ game_chooser.py
@@ -5,35 +5,56 @@ Resources:
 https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
 '''
 
-from helpers import centre_print
+'''
+TO DO
+
+-. Look into changing the the type formatting to something that gives me the Single Player and Genre separately
+from the multiplayer stuff. So it will basically show up as two list. One of genres and another of the multiplayer options.
+I could do this either in the class or in the list maker.
+
+-. First make the list of game types when the user inputs 1. Make it so that it shows in a neatly organised grid.
+
+-. After this, start work on the auto complete and the results.
+
+-. Make it presentable
+
+-. 
+
+-. 
+
+-. 
+
+-. 
+'''
+
+from helpers import terminal_center_print, center_string, square_frame
 from ANSI import ANSI
+from NintendoSwithGame import NintendoSwitchGame
+from nintendo_switch_games_list import my_games_list, list_maker
 
 
-centre_print(f'''
 
-||==========================================================||
-||                                                          ||
-||                ▄▄▄  ▄▄   ▄▄▄  ▐▄  ▄█ ▄▄                  ||
-||               ▐█ ▀█ ██   ▀▄ ▀  █▌█▌▀▐█ ▀                 ||
-||               ▄█▀▀█ ██   ▐▀▀ ▄  ██  ▄▀▀▀█▄               ||
-||               ▐█  ▐▌▐█▌▐▌▐█▄▄▌ ▐█ █▌▐█▄ ▐█               ||
-||                ▀  ▀  ▀▀▀  ▀▀▀  ▀▀ ▀▀ ▀▀▀▀                ||
-||              ▄▄    ▄▄▄           ▄▄▄   ▄▄                ||
-||             ▐█ ▀  ▐█ ▀█  ██ ▐███ ▀▄ ▀ ▐█ ▀               ||
-||             ▄█ ▀█▄▄█▀▀█ ▐█ ▌▐▌▐█ ▐▀▀ ▄▄▀▀▀█▄             ||
-||             ▐█▄ ▐█▐█  ▐▌██ ██▌▐█▌▐█▄▄▌▐█▄ ▐█             ||
-||              ▀▀▀▀  ▀  ▀ ▀▀  ▀ ▀▀▀ ▀▀▀  ▀▀▀▀              ||
-||                   (For Nintendo Switch)                  ||
-||                                                          ||
-||==========================================================||
+intro_text = '''
+   ▄▄▄  ▄▄   ▄▄▄  ▐▄  ▄█ ▄▄     
+  ▐█ ▀█ ██   ▀▄ ▀  █▌█▌▀▐█ ▀    
+  ▄█▀▀█ ██   ▐▀▀ ▄  ██  ▄▀▀▀█▄  
+  ▐█  ▐▌▐█▌▐▌▐█▄▄▌ ▐█ █▌▐█▄ ▐█  
+   ▀  ▀  ▀▀▀  ▀▀▀  ▀▀ ▀▀ ▀▀▀▀   
+ ▄▄    ▄▄▄           ▄▄▄   ▄▄   
+▐█ ▀  ▐█ ▀█  ██ ▐███ ▀▄ ▀ ▐█ ▀  
+▄█ ▀█▄▄█▀▀█ ▐█ ▌▐▌▐█ ▐▀▀ ▄▄▀▀▀█▄
+▐█▄ ▐█▐█  ▐▌██ ██▌▐█▌▐█▄▄▌▐█▄ ▐█
+ ▀▀▀▀  ▀  ▀ ▀▀  ▀ ▀▀▀ ▀▀▀  ▀▀▀▀ 
+ (For Nintendo Switch)
+'''
 
-''')
+print("\n\n\n\n")
 
-centre_print('''
-This is Alex's games chooser.
+terminal_center_print(square_frame(intro_text, 65, 0, "||", "="))
 
-''')
+terminal_center_print("This is Alex's games chooser.")
 
-a = input("Type a game category and I'll tell you which games are available in Alex's library of that type. You could also enter the number \"1\" to see all available types.\n\n:")
+# a = input("Type a game category and I'll tell you which games are available in Alex's library of that type. You could also enter the number \"1\" to see all available types.\n\n:")
 
-print("\n"+a)
+# for i in list_maker():
+#     print(i)
--- helpers.py
+++ helpers.py
@@ -4,8 +4,38 @@ Helpers
 
 import shutil
 
-def centre_print(text):
+def terminal_center_print(text):
     column_width, _ = shutil.get_terminal_size()
     lines = text.split("\n")
     for line in lines:
         print(line.center(column_width))
+
+
+def center_string(string, length, fill_char):
+    """
+    Centers a string within a given length, using a specified fill character.
+    """
+    # Calculate how much padding is needed on each side of the string
+    padding = length - len(string)
+    padding_left = padding // 2
+    padding_right = padding - padding_left
+    
+    # Build the final string by concatenating the fill character, the string, and the fill character
+    final_string = fill_char * padding_left + string + fill_char * padding_right
+
+    return final_string
+
+
+def square_frame(text, width, vertical_empty_lines = 0, vertical_line = "||", horizontal_line = "="):
+    final_text = ""
+    
+    final_text += vertical_line + center_string(horizontal_line, width - 2, horizontal_line) + vertical_line + "\n"
+    final_text += "".join([vertical_line + center_string(" ", width - 2, " ") + vertical_line  + "\n" for i in range(vertical_empty_lines)])
+    
+    lines = text.split("\n")
+    final_text += "".join([vertical_line + center_string(line, width - 2, " ") + vertical_line  + "\n" for line in lines])
+
+    final_text += "".join([vertical_line + center_string(" ", width - 2, " ") + vertical_line  + "\n" for i in range(vertical_empty_lines)])
+    final_text += vertical_line + center_string(horizontal_line, width - 2, horizontal_line) + vertical_line + "\n"
+
+    return final_text
